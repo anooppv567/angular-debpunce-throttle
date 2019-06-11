f6dedefe9125a2ddc7ef448a59b690167483fce6
anooppv567 <anoopit2010@gmail.com>
2019-06-11 18:49:09 -0500

    2 files modified,debounce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+userInput;
+constructor(){}
+  fetchData(){
+    console.log('Service called from UI');
+  }
+  betterFunction(execFunction,delay){
+    let timer;
+    return function(){
+      
+      if(timer){
+        clearTimeout(timer);
+      }
+       timer = setTimeout(function(){
+        execFunction();
+        },delay);
+      }
+    
+  }
+    betteFun = this.betterFunction(this.fetchData,5000);
+  keyUpPress(){
+    this.betteFun();
+  }
 }
